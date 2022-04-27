9ee8f9d35dc1a914e678aa2fd93ab69844e0c7d4
awxkee <qnljoy@gmail.com>
2022-04-27 11:11:14 +0200

    release 2

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "mozjpeg.swift",
-            targets: ["mozjpeg.swift", "mozjpeg.c"]),
+            targets: ["mozjpeg.swift"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
