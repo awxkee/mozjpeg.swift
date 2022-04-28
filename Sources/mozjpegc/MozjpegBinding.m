ed59fd0cac22c40817a89e3ade17f6a2633baa5f
awxkee <qnljoy@gmail.com>
2022-04-28 16:03:45 +0200

    added indefinite encoder

--- Sources/mozjpegc/MozjpegBinding.m
+++ Sources/mozjpegc/MozjpegBinding.m
@@ -12,14 +12,6 @@
 
 #define JPEG_LIB_VERSION 80
 
-struct my_error_mgr {
-  struct jpeg_error_mgr pub;    /* "public" fields */
-
-  jmp_buf setjmp_buffer;    /* for return to caller */
-};
-
-typedef struct my_error_mgr * my_error_ptr;
-
 /*
  * Here's the routine that will replace the standard error_exit method:
  */
@@ -27,18 +19,18 @@ typedef struct my_error_mgr * my_error_ptr;
 METHODDEF(void)
 my_error_exit (j_common_ptr cinfo)
 {
-  /* cinfo->err really points to a my_error_mgr struct, so coerce pointer */
-  my_error_ptr myerr = (my_error_ptr) cinfo->err;
-
-  /* Always display the message. */
-  /* We could postpone this until after returning, if we chose. */
-  (*cinfo->err->output_message) (cinfo);
-
-  /* Return control to the setjmp point */
-  longjmp(myerr->setjmp_buffer, 1);
+    /* cinfo->err really points to a my_error_mgr struct, so coerce pointer */
+    my_error_ptr myerr = (my_error_ptr) cinfo->err;
+    
+    /* Always display the message. */
+    /* We could postpone this until after returning, if we chose. */
+    (*cinfo->err->output_message) (cinfo);
+    
+    /* Return control to the setjmp point */
+    longjmp(myerr->setjmp_buffer, 1);
 }
 
-NSData * _Nullable compressJPEGData(UIImage * _Nonnull sourceImage, int quality) {
+uint8_t * createRGB8Buffer(UIImage * _Nonnull sourceImage) {
     int width = (int)(sourceImage.size.width * sourceImage.scale);
     int height = (int)(sourceImage.size.height * sourceImage.scale);
     
@@ -79,15 +71,105 @@ NSData * _Nullable compressJPEGData(UIImage * _Nonnull sourceImage, int quality)
     
     free(targetMemory);
     
+    return buffer;
+}
+
+@implementation JPEGCompression
+- (NSData * _Nonnull)finishCompress {
+    
+    jpeg_finish_compress(&cinfo);
+    
+    NSData *result = [[NSData alloc] initWithBytes:outBuffer length:outSize];
+    
+    jpeg_destroy_compress(&cinfo);
+    return result;
+}
+
+-(void* _Nullable) addEncoderImage:(UIImage*_Nonnull)sourceImage quality:(int)quality error:(NSError *_Nullable*_Nullable)error {
+    int width = (int)(sourceImage.size.width * sourceImage.scale);
+    int height = (int)(sourceImage.size.height * sourceImage.scale);
+    uint8_t* buffer = createRGB8Buffer(sourceImage);
+    if (width != self->width) {
+        *error = [[NSError alloc] initWithDomain:@"JPEGCompression" code:500 userInfo:@{ NSLocalizedDescriptionKey: NSLocalizedString(@"`addEncoderImage due to invalid image sizes` failed", nil) }];
+        return nil;
+    }
+    int bufferBytesPerRow = ((3 * (int)width) + 31) & (~31);
+    JSAMPROW rowPointer[1];
+    while (cinfo.next_scanline < cinfo.image_height) {
+        rowPointer[0] = (JSAMPROW)(buffer + cinfo.next_scanline * bufferBytesPerRow);
+        jpeg_write_scanlines(&cinfo, rowPointer, 1);
+    }
+    
+    free(buffer);
+    return nil;
+}
+
+-(void* _Nullable) createCompress:(UIImage*_Nonnull)sourceImage quality:(int)quality error:(NSError *_Nullable*_Nullable)error {
+    int width = (int)(sourceImage.size.width * sourceImage.scale);
+    int height = (int)(sourceImage.size.height * sourceImage.scale);
+    uint8_t* buffer = createRGB8Buffer(sourceImage);
+    int bufferBytesPerRow = ((3 * (int)width) + 31) & (~31);
+    
     struct jpeg_compress_struct cinfo;
     struct my_error_mgr jerr;
     cinfo.err = jpeg_std_error(&jerr.pub);
     if (setjmp(jerr.setjmp_buffer)) {
-      /* If we get here, the JPEG code has signaled an error.
-       * We need to clean up the JPEG object, close the input file, and return.
-       */
-      jpeg_destroy_compress(&cinfo);
-      return nil;
+        /* If we get here, the JPEG code has signaled an error.
+         * We need to clean up the JPEG object, close the input file, and return.
+         */
+        *error = [[NSError alloc] initWithDomain:@"JPEGCompression" code:500 userInfo:@{ NSLocalizedDescriptionKey: NSLocalizedString(@"`addEncoderImage` failed", nil) }];
+        jpeg_destroy_compress(&cinfo);
+        return nil;
+    }
+    jpeg_create_compress(&cinfo);
+    
+    uint8_t *outBuffer = NULL;
+    self->outBuffer = outBuffer;
+    self->outSize = 0;
+    jpeg_mem_dest(&cinfo, &outBuffer, &self->outSize);
+    
+    cinfo.image_width = (uint32_t)width;
+    cinfo.image_height = (uint32_t)height;
+    cinfo.input_components = 3;
+    cinfo.in_color_space = JCS_RGB;
+    jpeg_c_set_int_param(&cinfo, JINT_COMPRESS_PROFILE, JCP_FASTEST);
+    jpeg_set_defaults(&cinfo);
+    cinfo.arith_code = FALSE;
+    cinfo.dct_method = JDCT_ISLOW;
+    cinfo.optimize_coding = TRUE;
+    jpeg_set_quality(&cinfo, quality, 1);
+    jpeg_simple_progression(&cinfo);
+    jpeg_start_compress(&cinfo, 1);
+    
+    JSAMPROW rowPointer[1];
+    while (cinfo.next_scanline < cinfo.image_height) {
+        rowPointer[0] = (JSAMPROW)(buffer + cinfo.next_scanline * bufferBytesPerRow);
+        jpeg_write_scanlines(&cinfo, rowPointer, 1);
+    }
+    
+    free(buffer);
+    
+    self->width = width;
+    return nil;
+}
+
+@end
+
+NSData * _Nullable compressJPEGData(UIImage * _Nonnull sourceImage, int quality) {
+    int width = (int)(sourceImage.size.width * sourceImage.scale);
+    int height = (int)(sourceImage.size.height * sourceImage.scale);
+    uint8_t* buffer = createRGB8Buffer(sourceImage);
+    int bufferBytesPerRow = ((3 * (int)width) + 31) & (~31);
+    
+    struct jpeg_compress_struct cinfo;
+    struct my_error_mgr jerr;
+    cinfo.err = jpeg_std_error(&jerr.pub);
+    if (setjmp(jerr.setjmp_buffer)) {
+        /* If we get here, the JPEG code has signaled an error.
+         * We need to clean up the JPEG object, close the input file, and return.
+         */
+        jpeg_destroy_compress(&cinfo);
+        return nil;
     }
     jpeg_create_compress(&cinfo);
     
