e5534764815548e6e3ffc5171619b75c30fd6452
awxkee <qnljoy@gmail.com>
2022-05-19 00:27:39 +0200

    incremental tj decoder

--- Sources/mozjpegc/MozjpegBinding.m
+++ Sources/mozjpegc/MozjpegBinding.m
@@ -8,6 +8,7 @@
 #import <Foundation/Foundation.h>
 #import "MozjpegBinding.h"
 #import "jpeglib.h"
+#import "turbojpeg.h"
 
 /*
  * Here's the routine that will replace the standard error_exit method:
@@ -286,3 +287,60 @@ NSData * _Nullable compressJPEGData(MozjpegImage * _Nonnull sourceImage, int qua
     
     return result;
 }
+
+@implementation MozjpegDecompress {
+    tjhandle decompressPtr;
+}
+
+-(id)init {
+    decompressPtr = tjInitDecompress();
+}
+
+-(void)dealloc {
+    if (decompressPtr) {
+        tjDestroy(decompressPtr);
+    }
+    decompressPtr = NULL;
+}
+
+-(nullable MozjpegImage*)decompress:(nonnull NSData*)chunk {
+    int width = 0;
+    int height = 0;
+    int jpegSubsample = 0;
+    int jpegColorspace = 0;
+    int result = tjDecompressHeader3(decompressPtr, (unsigned char *)chunk.bytes, chunk.length, &width, &height, &jpegSubsample, &jpegColorspace);
+    if (result) {
+        //some error
+        return nil;
+    }
+    unsigned char* outputBuffer = (unsigned char* )malloc(width * height * 4);
+    result = tjDecompress2(decompressPtr, (unsigned char *)chunk.bytes, chunk.length, outputBuffer, width, 0, height, TJPF_RGBA, 0);
+    if (result) {
+        //some error
+        free(outputBuffer);
+        return nil;
+    }
+    
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    int flags = kCGBitmapByteOrder32Big | kCGImageAlphaPremultipliedLast;
+    CGContextRef gtx = CGBitmapContextCreate(outputBuffer, width, height, 8, width * 4, colorSpace, flags);
+    if (gtx == NULL) {
+        free(outputBuffer);
+        return nil;
+    }
+    CGImageRef imageRef = CGBitmapContextCreateImage(gtx);
+    MozjpegImage *image = nil;
+#if TARGET_OS_OSX
+    image = [[NSImage alloc] initWithCGImage:imageRef size:CGSizeZero];
+#else
+    image = [UIImage imageWithCGImage:imageRef scale:1 orientation: UIImageOrientationUp];
+#endif
+
+    CGImageRelease(imageRef);
+    CGColorSpaceRelease(colorSpace);
+    
+    free(outputBuffer);
+    return image;
+}
+
+@end
