017e6e3b4268c2082312b5b4d96afd3e6d77c708
awxkee <qnljoy@gmail.com>
2022-05-20 21:46:37 +0200

    macos catalyst + some improvements

--- Sources/mozjpegc/MozjpegImage.mm
+++ Sources/mozjpegc/MozjpegImage.mm
@@ -88,62 +88,46 @@
 }
 
 -(nonnull uint8_t *) createRGB8Buffer {
-//    int width = self.mjIntrinsicWidth;
-//    int height = self.mjIntrinsicHeight;
-//    int targetBytesPerRow = ((4 * (int)width) + 31) & (~31);
-//    uint8_t *targetMemory = static_cast<uint8_t*>(malloc((int)(targetBytesPerRow * height)));
-//
-//    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-//    CGBitmapInfo bitmapInfo = kCGImageAlphaNoneSkipFirst | kCGBitmapByteOrder32Host;
-//
-//    CGContextRef targetContext = CGBitmapContextCreate(targetMemory, width, height, 8, targetBytesPerRow, colorSpace, bitmapInfo);
-//
-//    UIGraphicsPushContext(targetContext);
-//
-//    CGColorSpaceRelease(colorSpace);
-//
-//    CGContextDrawImage(targetContext, CGRectMake(0, 0, width, height), self.CGImage);
-//
-//    UIGraphicsPopContext();
-//
-//    int bufferBytesPerRow = ((3 * (int)width) + 31) & (~31);
-//    uint8_t *buffer = static_cast<uint8_t*>(malloc(bufferBytesPerRow * height));
-//
-//    for (int y = 0; y < height; y++) {
-//        for (int x = 0; x < width; x++) {
-//            uint32_t *color = ((uint32_t *)&targetMemory[y * targetBytesPerRow + x * 4]);
-//
-//            uint32_t r = ((*color >> 16) & 0xff);
-//            uint32_t g = ((*color >> 8) & 0xff);
-//            uint32_t b = (*color & 0xff);
-//
-//            buffer[y * bufferBytesPerRow + x * 3 + 0] = r;
-//            buffer[y * bufferBytesPerRow + x * 3 + 1] = g;
-//            buffer[y * bufferBytesPerRow + x * 3 + 2] = b;
-//        }
-//    }
-//
-//    CGContextRelease(targetContext);
-//
-//    free(targetMemory);
-//
-//    return buffer;
-    CGImageRef imageRef = [self makeCGImage];
-    NSUInteger width = CGImageGetWidth(imageRef);
-    NSUInteger height = CGImageGetHeight(imageRef);
+    int width = self.mjIntrinsicWidth;
+    int height = self.mjIntrinsicHeight;
+    int targetBytesPerRow = ((4 * (int)width) + 31) & (~31);
+    uint8_t *targetMemory = static_cast<uint8_t*>(malloc((int)(targetBytesPerRow * height)));
+
     CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    unsigned char *rawData = (unsigned char*) calloc(height * width * 4, sizeof(unsigned char));
-    NSUInteger bytesPerPixel = 3;
-    NSUInteger bytesPerRow = bytesPerPixel * width;
-    NSUInteger bitsPerComponent = 8;
-    CGContextRef context = CGBitmapContextCreate(rawData, width, height,
-                                                 bitsPerComponent, bytesPerRow, colorSpace,
-                                                 kCGImageAlphaNone | kCGBitmapByteOrder32Big);
+    CGBitmapInfo bitmapInfo = kCGImageAlphaNoneSkipFirst | kCGBitmapByteOrder32Host;
+
+    CGContextRef targetContext = CGBitmapContextCreate(targetMemory, width, height, 8, targetBytesPerRow, colorSpace, bitmapInfo);
+
+    UIGraphicsPushContext(targetContext);
+
     CGColorSpaceRelease(colorSpace);
-    
-    CGContextDrawImage(context, CGRectMake(0, 0, width, height), imageRef);
-    CGContextRelease(context);
-    return rawData;
+
+    CGContextDrawImage(targetContext, CGRectMake(0, 0, width, height), self.CGImage);
+
+    UIGraphicsPopContext();
+
+    int bufferBytesPerRow = ((3 * (int)width) + 31) & (~31);
+    uint8_t *buffer = static_cast<uint8_t*>(malloc(bufferBytesPerRow * height));
+
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            uint32_t *color = ((uint32_t *)&targetMemory[y * targetBytesPerRow + x * 4]);
+
+            uint32_t r = ((*color >> 16) & 0xff);
+            uint32_t g = ((*color >> 8) & 0xff);
+            uint32_t b = (*color & 0xff);
+
+            buffer[y * bufferBytesPerRow + x * 3 + 0] = r;
+            buffer[y * bufferBytesPerRow + x * 3 + 1] = g;
+            buffer[y * bufferBytesPerRow + x * 3 + 2] = b;
+        }
+    }
+
+    CGContextRelease(targetContext);
+
+    free(targetMemory);
+
+    return buffer;
 }
 
 -(int)mjIntrinsicWidth {
